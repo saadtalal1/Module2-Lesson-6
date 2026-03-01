ac12059a4addd5909d93716a4dbb9e9edd05c092
saadtalal1 <saadtalal0112@gmail.com>
2026-03-01 11:04:30 +0400

    Add files via upload

---
+++ ACP.py
@@ -0,0 +1,9 @@
+import turtle
+turtle.Screen().bgcolor("black")
+turtle.Screen().setup(300,400)
+turtle.Turtle()
+turtle.color("white")
+turtle.goto(0,0)
+for i in range(4):
+    turtle.forward(100)
+    turtle.right(90)
